42d2a3faa56ce9bdbd950d4e46fb7e43b40e373c
Folsten <mega4642179@gmail.com>
2021-02-15 23:01:11 +0300

    introduced gulp-babel and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,35 @@
 const GulpClient = require('gulp');
 const { series, parallel, src, dest, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 const minify = require('gulp-minify');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 function scssCompiler() {
   return src('src/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
     .pipe(dest('build/css'))
     .pipe(dest('src/css'))
     .pipe(browserSync.stream())
-    // .pipe(cb())
 }
 
 function jsCompiler() {
   return src('src/js/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(minify())
     .pipe(dest('build/js'))
-    // .pipe(cb())
 }
 
 function imgCompiler() {
   return src('src/img/**/*')
   .pipe(imagemin())
   .pipe(dest('build/img'))
-  // .pipe(cb())
 }
 
 function devServer() {
@@ -39,4 +42,5 @@ function devServer() {
 }
 
 
-exports.build = series(devServer)
+exports.dev = series(devServer)
+exports.build = series(scssCompiler, jsCompiler, imgCompiler)
