be295900c1d88db28395746ba86cd8d6282e353c
Folsten <mega4642179@gmail.com>
2021-04-15 22:53:39 +0300

    changed commited description of the stylelint file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 // Use it only if you plan to use Tailwind CSS in your project, otherwise you may delete this.
-// This extension allow you to avoid wrong errors while using Tailwind CSS.
+// This extension as linter allow you to avoid wrong errors while using Tailwind CSS.
 // To activate this configuration you need to install "stylelint" extension in your code editor.
+// P.S This is just linter, not a part of any functionality.
 
 module.exports = {
   ignoreFiles: ["**/*.sass"],
