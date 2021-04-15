0a8d31ab59f7c4d156978c0e5c22695109229665
Folsten <mega4642179@gmail.com>
2021-04-15 22:51:41 +0300

    added ignoreFiles for sass and added the description on what file is it

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,9 @@
+// Use it only if you plan to use Tailwind CSS in your project, otherwise you may delete this.
+// This extension allow you to avoid wrong errors while using Tailwind CSS.
+// To activate this configuration you need to install "stylelint" extension in your code editor.
+
 module.exports = {
+  ignoreFiles: ["**/*.sass"],
   extends: ["stylelint-config-standard"],
   rules: {
     "at-rule-no-unknown": [
